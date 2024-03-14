381c134dcbac6f022e6057952bb0bc124c463199
rauldiekmann <raulpujante@gmail.com>
2024-03-14 11:40:01 +0100

    Creado el header
    Falta implementar la funcionalidad de búsqueda en la barra

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {FaSearch} from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className='bg-rose-800 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+          <Link to="/">
+            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                <span className='text-neutral-50'>ViviendaJoven</span>
+                <span className='text-yellow-400'>CARM</span>
+            </h1>
+          </Link>
+          <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+            <input type="text" placeholder='Buscar propiedad...' 
+            className='bg-transparent focus:outline-none w-24 sm:w-64' />
+            <FaSearch className='text-slate-600' />
+          </form>
+          <ul className='flex gap-4'>
+
+            <Link to="/">
+              <li className='hidden sm:inline text-neutral-50 hover:underline'>Inicio</li>
+            </Link>
+
+            <Link to="/about">
+              <li className='hidden sm:inline text-neutral-50 hover:underline'>Informacion</li>
+            </Link>
+
+            <Link to="/sign-in">
+              <li className='sm:inline text-neutral-50 hover:underline'>Acceder</li>
+            </Link>
+            
+          </ul>
+        </div>
+        
+    </header>
+  )
+}
+
+export default Header
