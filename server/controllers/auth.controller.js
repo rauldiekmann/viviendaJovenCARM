c3c61258e02ac1d31c0f892b0ac907d89e1d21e3
rauldiekmann <raulpujante@gmail.com>
2024-03-14 17:46:48 +0100

    Creado middleware de manejo de errores
    Junto a middleware propio  que sevirá como utilidad para diagnosticar errores durante el desarrollo

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import {errorHandler } from "../utils/errorHandler.js"
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
     const hashedPassword = await bcryptjs.hash(password, 12);
     const newUser = new User({username, email, password: hashedPassword});
@@ -9,9 +10,7 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json("Registrado con éxito");
     } catch (error) {
-      res.status(500).json(error.message);  
+      next(error);
     }
-    await newUser.save()
-    res.status(201).json("Registrado con éxito")
 };
 
--- server/index.js
+++ server/index.js
@@ -19,6 +19,20 @@ app.use('/server/user', userRouter);
 app.use('/server/auth', authRouter);
 
 
+//Middleware para errores
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error";
+    return res.status(statusCode).json({
+      success:false,
+      statusCode,
+      message,  
+    });
+});
+
+
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
