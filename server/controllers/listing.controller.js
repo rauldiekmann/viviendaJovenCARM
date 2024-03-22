ff418dd839e1763eff068618600ef0e68a54748f
rauldiekmann <raulpujante@gmail.com>
2024-03-22 15:51:32 +0100

    Actualizar propiedad
    -Ruta y función de controlador para actualizar inmueble
    -Funcionalidad para actualizarlo en el Front End

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -27,3 +27,38 @@ export const deleteListing = async (req, res, next) => {
         next(error);
     }
 }
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+        return res.status(404).json("Inmueble no encontrado");
+    }
+
+    if (req.user.id !== listing.userRef) {
+        return res.status(401).json("Solo puedes actualizar tus inmuebles");
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        return res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getListing = async (req, res, next) => {
+   try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+        return res.status(404).json("Inmueble no encontrado");
+    }
+    return res.status(200).json(listing);
+   } catch (error) {
+    next(error);
+   } 
+}
