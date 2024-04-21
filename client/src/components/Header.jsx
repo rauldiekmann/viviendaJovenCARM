045425a0552f17834b0c0577d143c97b4d802ef6
rauldiekmann <raulpujante@gmail.com>
2024-04-21 10:12:03 +0200

    "Terminado listado de búsqueda"

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import {FaSearch} from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
 
 const Header = () => {
 
   const {currentUser} = useSelector(state => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //mantener los parametros definidos por los filtros en la query string 
+    //al ejecutar nueva busqueda
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  }
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }}, [location.search]);
 
   return (
     <header className='bg-rose-800 shadow-md'>
@@ -16,9 +35,13 @@ const Header = () => {
                 <span className='text-yellow-400'>CARM</span>
             </h1>
           </Link>
-          <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+          <form onSubmit = {handleSubmit}
+          className='bg-slate-100 p-3 rounded-lg flex items-center'>
             <input type="text" placeholder='Buscar propiedad...' 
-            className='bg-transparent focus:outline-none w-24 sm:w-64' />
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            />
             <FaSearch className='text-slate-600' />
           </form>
           <ul className='flex gap-4'>
